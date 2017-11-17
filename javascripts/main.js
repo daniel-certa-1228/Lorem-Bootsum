787d51e4e4caff5d1ae0d1c0cb2b348024b12ab9
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-17 13:59:10 -0600

    ajax loading

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,25 +2,37 @@
 console.log("main.js");
 
 {
-var Bootsum = {};
+    var Bootsum = {};
 }
 
 $( document ).ready(function() {
 
+    let bootsumArray = [];
+    
+     Bootsum.getBootsum = () => {
+        let bootsum =  $.ajax({url:"../data/lorem_bootsum.json"});
+        return bootsum;
+    };
+
+    Bootsum.loadBootsum = () => {
+        Bootsum.getBootsum().done(function(data) {
+            let bootArray = Object.values(Object.values(data)[0]);
+            // console.log(bootArray);
+            for (let i = 0; i< bootArray.length; i++) {
+                bootsumArray.push(bootArray[i]);
+            } // return bootArray;
+        });
+    };
+    Bootsum.loadBootsum();
+    console.log(`full`, bootsumArray);
+/////////////////////////////////////////////////////
+
     {
-        Bootsum.loadBootsum = () => {
-
-            let bootsumArray = [];
-
-            $.ajax({url:"../data/lorem_bootsum.json"}).done(bootsumLoadComplete).fail(function(error) {
-                console.log("error");
-            });
-
-            function bootsumLoadComplete(json) {
-                let bootsumArray = Object.values(Object.values(json)[0]);
-                console.log(bootsumArray);
-            }
-        };
-        Bootsum.loadBootsum();
+        let random1 = Math.floor(Math.random()*10);
+        console.log(random1);
+        // console.log(Bootsum.returnBootsumArray());
     }
+
 });
+
+
---
+++ javascripts/randomizer.js
@@ -0,0 +1,15 @@
+// 'use strict';
+// console.log("randomizer.js");
+
+// $( document ).ready(function() {
+
+//     {
+//         let random1 = Math.floor(Math.random()*10);
+//         console.log(random1);
+
+//         Bootsum.randomSentence = (num) => {
+//             // console.log(num);
+//         };
+//     }
+
+// });
