9dd07815a0be9a0ee39128745e72d985ec7b55b2
daniel-certa-1228 <mfdancerta@gmail.com>
2017-11-18 09:09:43 -0600

    added html and scss

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,10 +9,17 @@ var bootsumArray = [];
 loader.loadBootsum()
     .then((bootsum) => {
         let boots = Object.values(Object.values(bootsum)[0]);
-        console.log(boots);
+        // console.log(boots);
         for (let i = 0; i < boots.length; i++) {
             bootsumArray.push(boots[i]); 
         }
-        console.log(bootsumArray[0]);
+        // console.log(bootsumArray.length);
+        return bootsumArray;
+    })
+    .then((bootsumArray) => {
+        console.log(bootsumArray.length);
+        let random1 = Math.floor(Math.random()*(bootsumArray.length));
+        console.log(bootsumArray[random1]);
+
     });
 
