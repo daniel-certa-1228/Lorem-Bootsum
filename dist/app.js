050f77b88acce4569bb1f220dea4dc9ed67bb020
Dan Certa <daniel.certa.1228@gmail.com>
2017-11-19 19:55:45 -0600

    changed path to json

--- dist/app.js
+++ dist/app.js
@@ -40,7 +40,7 @@ var bootsumLoader = {};
 bootsumLoader.loadBootsum = () => {
     return new Promise(function(resolve, reject){
         let getBoot = new XMLHttpRequest();
-        getBoot.open("GET", "../data/lorem_bootsum.json");
+        getBoot.open("GET", "https://daniel-certa-1228.github.io/Lorem-Bootsum/data/lorem_bootsum.json");
         getBoot.send();
         getBoot.addEventListener("load", (event) => {
             let bootsum = JSON.parse(event.target.responseText);
